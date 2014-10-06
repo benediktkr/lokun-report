1b351db74c98da452edf4513d9b35abf19a69564
Benedikt Krisinsson <benedikt@lokun.is>
2014-10-06 20:00:33 +0000

    revert

--- report/common.py
+++ report/common.py
@@ -4,10 +4,11 @@
 from datetime import datetime
 from config import log_to_file, logfile
 
-def debug(msg, _=False):
+def debug(msg, error=False):
     s = str(datetime.now().replace(microsecond=0)) + "\t" + str(msg)
     print s
-    f = open(logfile, "a")
-    f.write(s + "\n")
-    f.close()
+    if log_to_file or error:
+        f = open(logfile, "a")
+        f.write(s + "\n")
+        f.close()
 
